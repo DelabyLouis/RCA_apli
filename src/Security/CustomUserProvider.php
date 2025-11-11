688f04aedb378beab4b52c867f3a472ac87ff494
Louis Delaby <delaby.louis@gmail.com>
2025-11-11 14:17:16 +0100

    Add debug log in CustomUserProvider refreshUser method

--- src/Security/CustomUserProvider.php
+++ src/Security/CustomUserProvider.php
@@ -54,6 +54,9 @@ class CustomUserProvider implements UserProviderInterface
             throw new UserNotFoundException(sprintf('User with ID "%s" not found or disabled.', $user->getIdUser()));
         }
 
+        // Log pour debug
+        error_log("CustomUserProvider::refreshUser - User refreshed successfully: " . $refreshedUser->getUsername());
+
         return $refreshedUser;
     }
 
